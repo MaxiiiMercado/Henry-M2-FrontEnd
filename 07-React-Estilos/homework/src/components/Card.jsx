aa8954fcef39c562efba46a424ae783b86ad8fd8
MaxiiiMercado95 <MaxiiiMercado95@gmail.com>
2022-10-19 00:00:09 -0300

    homework | React-Estilos

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import styles from './Card.module.css';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose }) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={styles.cardContainer}>
+      <span>
+        <button onClick={onClose}>X</button>
+      </span>
+      <h3>{name}</h3>
+      <div className={styles.tempContainer}>
+        <div>
+          <h4>Min</h4>
+          <h4>{min}</h4>
+        </div>
+        <div>
+          <h4>Max</h4>
+          <h4>{max}</h4>
+        </div>
+        <div>
+          <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Clima" />
+        </div>
+      </div>
+    </div>
+  )
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import styles from './SearchBar.module.css'
 
-export default function SearchBar(props) {
+export default function SearchBar({ onSearch }) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div className={styles.SearchContainer}>
+      <input type="text" name="inputCiudad" placeholder="Ciudad..." />
+      <button onClick={() => onSearch('Buscando ciudad')}>Agregar</button>
+    </div>
+  )
 };
